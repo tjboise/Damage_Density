ef101f49f2af41b2ac77854295c2c2e91dde79bf
Tianjie(TJ) Zhang <tjzhang@u.boisestate.edu>
2022-02-09 12:01:03 -0700

    1

--- DamageDensity/gcul.m
+++ DamageDensity/gcul.m
@@ -1,8 +1,42 @@
 %% This code mainly calculates the damage density
-%% read image
+%% read imageN = 500; % 每个维度的数据点数
+% 网格化x,y二维空间
+[X,Y] = meshgrid(linspace(xmin,xmax,N),linspace(ymin,ymax,N));
+% 采用插值法扩展数据，可用方法有'linear'(default)|'nearest'|'natural'|'cubic'|'v4'|
+Z = griddata(x,y,z,X,Y,'v4');
+%% 等高线法
+figure('NumberTitle','off','Name','等高线法','Color','w','MenuBar','none','ToolBar','none');
+contourf(X,Y,Z,N, 'LineColor','none');
+colormap('jet');
+colorbar;
+axis off;
+%% 投影图法
+figure('NumberTitle','off','Name','投影图法','Color','w','MenuBar','none','ToolBar','none');
+surf(X,Y,Z,'LineStyle','none');
+xlim([min(X(:)) max(X(:))]);
+ylim([min(Y(:)) max(Y(:))]);
+axis off;
+colormap('jet');
+colorbar;
+shading interp;
+view(0,90);
+%% imagesc法
+figure('NumberTitle','off','Name','imagesc法','Color','w','MenuBar','none','ToolBar','none');
+% 因为图像坐标和笛卡尔坐标起始位置不一样，需要上下翻转
+imagesc(flipud(Z));
+colormap('jet');
+colorbar;
+axis off;
+%% pcolor法
+figure('NumberTitle','off','Name','pcolor法','Color','w','MenuBar','none','ToolBar','none');
+pcolor(X,Y,Z);
+colormap('jet');
+colorbar;
+shading interp;
+axis off;
 [file_name, pathname] = uigetfile( '*.*','choose a file');
 I1 = imread([pathname,'/',file_name]); 
-dirc='test2';
+dirc='test1';
 %% crop the ROI
 [m, n, z] = size(I1);
 figure(1),imshow(I1);
@@ -38,7 +72,9 @@ I1=rgb2gray(I1);
 A=double(I1);
 [GX,GY]=gradient(A);
 G=sqrt(GX.*GX+GY.*GY);
-mean_gradient=mean(G(x));% get the mean gradient
+mean_gradient=mean(G);
+
+mean_gradient1=mean(G(x));% get the mean gradient
 sum_gradient=sum(G(x));
 figure(4),imshow(G);
 imwrite(G,[pathname,dirc,'/Gradientoriginal_',file_name]);
